115e4eb1b35cb3b6b966f0042373411bd1a6ec95
jungry <seojungrain@gmail.com>
2022-10-20 15:23:04 +0900

    first commit

---
+++ src/main/java/com/joongbu/fakerly/FakerlyApplication.java
@@ -0,0 +1,13 @@
+package com.joongbu.fakerly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakerlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakerlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joongbu/fakerly/FakerlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongbu.fakerly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakerlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
