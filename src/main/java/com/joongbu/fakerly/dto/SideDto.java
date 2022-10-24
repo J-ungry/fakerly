33ce6e886a8ef898d614774c74db9846d00fda8b
jungry <seojungrain@gmail.com>
2022-10-24 16:03:23 +0900

    1024

---
+++ src/main/java/com/joongbu/fakerly/dto/SideDto.java
@@ -0,0 +1,39 @@
+package com.joongbu.fakerly.dto;
+
+import java.util.Date;
+
+
+import lombok.Data;
+
+/*
+ +---------------+--------------+------+-----+-------------------+-------------------+
+| Field         | Type         | Null | Key | Default           | Extra             |
++---------------+--------------+------+-----+-------------------+-------------------+
+| side_board_no | int          | NO   | PRI | NULL              | auto_increment    |
+| title         | varchar(100) | NO   |     | NULL              |                   |
+| summ          | varchar(255) | NO   |     | NULL              |                   |
+| intro         | varchar(255) | NO   |     | NULL              |                   |
+| purpose       | varchar(50)  | NO   |     | NULL              |                   |
+| sup           | varchar(100) | NO   |     | NULL              |                   |
+| attend        | varchar(50)  | NO   |     | NULL              |                   |
+| state         | tinyint(1)   | NO   |     | 1                 |                   |
+| post_time     | datetime     | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
+| report        | int          | NO   |     | 0                 |                   |
+| user_no       | int          | YES  | MUL | NULL              |                   |
++---------------+--------------+------+-----+-------------------+-------------------+
+ */
+@Data
+public class SideDto {
+
+	private int sideBoardNo;
+	private String title;   
+	private String summ;         
+	private String intro;        
+	private String purpose;      
+	private String sup;        
+	private String attend;       
+	private int state;        
+	private Date post_time;
+	private int report;   
+    
+}
