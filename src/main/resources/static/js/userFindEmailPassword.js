35c9d186d95a8d4f4a974bafbe678254c5e5063f
Fancyee <74527909+kuma1105@users.noreply.github.com>
2022-10-26 17:02:20 +0900

    JS 코드 수정

--- src/main/resources/static/js/userFindEmailPassword.js
+++ src/main/resources/static/js/userFindEmailPassword.js
@@ -9,17 +9,17 @@ const phoneNbChkP = document.querySelector("#phoneNbChkP");
 phoneE.onblur = function(e) {
 	if (phoneE.value.length != 11) {
 		console.log("전화번호 11자리를 입력해주세요.");
-		phoneNbChkE.innerHTML = "전화번호 11자리를 입력해주세요.";
+		phoneNbChkE.textContent = "전화번호 11자리를 입력해주세요.";
 	} else {
-		phoneNbChkE.innerHTML = "";
+		phoneNbChkE.textContent = "";
 	}
 }
 
 phoneP.onblur = function(e) {
 	if (phoneP.value.length != 11) {
 		console.log("전화번호 11자리를 입력해주세요.");
-		phoneNbChkP.innerHTML = "전화번호 11자리를 입력해주세요.";
+		phoneNbChkP.textContent = "전화번호 11자리를 입력해주세요.";
 	} else {
-		phoneNbChkP.innerHTML = "";
+		phoneNbChkP.textContent = "";
 	}
 }
