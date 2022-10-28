58413f9a77ff5c634b403dca954563277e29167d
Fancyee <74527909+kuma1105@users.noreply.github.com>
2022-10-28 14:54:00 +0900

    이메일 유효성 검사 기능 추가

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,26 +1,18 @@
 
 console.log("login.js");
 
-const phoneE = document.querySelector("#phoneE");
-const phoneP = document.querySelector("#phoneP");
-const phoneNbChkE = document.querySelector("#phoneNbChkE");
-const phoneNbChkP = document.querySelector("#phoneNbChkP");
+const emailLoginInput = document.querySelector(".emailLoginInput");
+const emailChkLogin = document.querySelector("#emailChkLogin");
 
-phoneE.onblur = function(e) {
-	if (phoneE.value.length != 11) {
-		console.log("전화번호 11자리를 입력해주세요.");
-		phoneNbChkE.textContent = "전화번호 11자리를 입력해주세요.";
-	} else {
-		phoneNbChkE.textContent = "";
-	}
-}
+const emailValue = new RegExp(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]+$/);
 
-phoneP.onblur = function(e) {
-	if (phoneP.value.length != 11) {
-		console.log("전화번호 11자리를 입력해주세요.");
-		phoneNbChkP.textContent = "전화번호 11자리를 입력해주세요.";
+emailLoginInput.onblur = function(e) {
+	console.log("커서가 밖을 나갔습니다.");
+	if (!emailValue.test(emailLoginInput.value)) {
+		console.log("이메일 양식에 맞게 입력해주세요.");
+		emailChkLogin.textContent = "이메일 양식에 맞게 입력해주세요.";
 	} else {
-		phoneNbChkP.textContent = "";
+		emailChkLogin.textContent = "";
 	}
 }
 
