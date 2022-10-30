88b36ea0fc2895dc94afd9b04dcb05f18e2ce78f
J <rntod23@daum.net>
2022-10-31 05:58:34 +0900

    whffudy

---
+++ src/main/java/com/joongbu/fakerly/mapper/ReQnaMapper.java
@@ -0,0 +1,11 @@
+package com.joongbu.fakerly.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.joongbu.fakerly.dto.ReQnaDto;
+@Mapper
+public interface ReQnaMapper {
+	int insert(ReQnaDto qa_reply_no);
+	int update(ReQnaDto qa_reply_no);
+	int delete(int reply);
+}
