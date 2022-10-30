2a1cdb8cf5b41612be36e9706e286cdda55b08a4
kth <kim369346@naver.com>
2022-10-30 22:45:26 +0900

    mainboard kth

--- src/main/resources/static/js/mainboard/mainboardSearch.js
+++
@@ -1,9 +0,0 @@
-$('#searching').keyup(function() {
-	var searching=$('#searching').val();
-	console.log(searching);
-	$("#container:contains('"+searching+"')").each(function() {
-		var regex = new RegExp(searching,'gi');
-		$(this).html( $(this).text().replace(regex, "<span><strong style=\"box-shadow: inset 0 -20px 0 #bfffa1;\">"+searching+"</strong></span>") );
-	})
-	
-})
